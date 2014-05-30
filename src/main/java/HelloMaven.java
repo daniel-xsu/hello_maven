a01b5ee9ddc4d4d7f30e46834123da0a9f8399f0
Leonid Khelemes <leonid.khelemes@engagepoint.com>
2014-05-30 18:44:43 +0300

    "first commit"

---
+++ src/main/java/HelloMaven.java
@@ -0,0 +1,9 @@
+/**
+ * Created by leonid.khelemes on 28.05.2014.
+ */
+public class HelloMaven {
+
+    public static void main (String args[]){
+        System.out.println("Hello Maven!");
+    }
+}
